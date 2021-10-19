9af12ce237a4161c3902f063ff56b8e0086b7d49
Faizan Ullah <faizanullah1999@gmail.com>
2021-10-19 17:15:24 +0500

    Synced Form Complete

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,12 @@
             Form is synced with Firestore
         </div>
         <div class="notification is-link" v-if="state === 'modified'">
-            Form data changed, will sync with Firebase
+            <span
+                class="spinner-grow spinner-grow-sm"
+                role="status"
+                aria-hidden="true"
+            ></span>
+            <span class="">Form data changed, will sync with Firebase</span>
         </div>
         <div class="notification is-warning" v-else-if="state === 'revoked'">
             From data and Firebase revoked to original data
@@ -26,39 +31,68 @@
             Failed to save to Firestore. {{ errorMessage }}
         </div>
         <div class="notification is-info" v-else-if="state === 'loading'">
-            Loading...
+            <span
+                class="spinner-grow spinner-grow-sm"
+                role="status"
+                aria-hidden="true"
+            ></span>
+            <!-- <span class="">Loading...</span> -->
         </div>
         <form @submit.prevent="updateFirebase" @input="fieldUpdate">
-            <input
-                placeholder="name"
-                type="text"
-                name="name"
-                id="name"
-                v-model="formData.name"
-            />
-            <input
-                placeholder="email"
-                type="email"
-                name="email"
-                id="email"
-                v-model="formData.email"
-            />
-            <input
-                placeholder="phone"
-                type="tel"
-                name="phone"
-                id="phone"
-                v-model="formData.phone"
-            />
-            <button type="submit">SUBMIT</button>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">NAME</span>
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="name"
+                    aria-label="name"
+                    aria-describedby="basic-addon1"
+                    name="name"
+                    id="name"
+                    v-model="formData.name"
+                />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">EMAIL</span>
+                <input
+                    type="email"
+                    class="form-control"
+                    placeholder="email"
+                    aria-label="email"
+                    aria-describedby="basic-addon1"
+                    name="email"
+                    id="email"
+                    v-model="formData.email"
+                />
+            </div>
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">PHONE</span>
+                <input
+                    type="tel"
+                    class="form-control"
+                    placeholder="phone"
+                    aria-label="phone"
+                    aria-describedby="basic-addon1"
+                    name="phone"
+                    id="phone"
+                    v-model="formData.phone"
+                />
+            </div>
+            <button type="submit" class="btn btn-primary m-2 p-1 px-2">
+                SUBMIT
+            </button>
+            <!-- <button type="submit">SUBMIT</button> -->
         </form>
-        <button @click="revertToOriginal">REVERT</button>
+        <button @click="revertToOriginal" class="btn btn-danger m-2 px-3">
+            REVERT
+        </button>
     </div>
 </template>
 
 <script>
 import { db } from "./firebase";
 import { debounce } from "debounce";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const documentPath = "contacts/faizan";
 
@@ -98,8 +132,11 @@ export default {
             this.updateFirebase();
         }, 1500),
         revertToOriginal() {
-            this.state = "revoked";
+            this.state = "loading";
             this.formData = { ...this.originalData };
+            this.firebaseData = { ...this.originalData };
+            this.updateFirebase();
+            this.state = "revoked";
         },
     },
     created: async () => {
@@ -151,6 +188,10 @@ form {
     display: flex;
     flex-direction: column;
     align-items: center;
+}
+
+.input-group {
+    max-width: 400px;
 }
 
 form > * {
